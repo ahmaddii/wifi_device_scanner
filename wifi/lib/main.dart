75cc25e5f558b0c0ae19cfc5caa4d1aa71d0710d
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2026-01-12 22:08:54 +0500

    main changes

--- wifi/lib/main.dart
+++ wifi/lib/main.dart
@@ -48,7 +48,7 @@ class WifiScannerApp extends StatelessWidget {
           style: ElevatedButton.styleFrom(
             elevation: 2,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(13),
             ),
           ),
         ),
